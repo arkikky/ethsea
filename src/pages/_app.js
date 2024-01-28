60eccd3566db4c0261198bc93055310900020412
Dicky Pratama A <112843504+arkikky@users.noreply.github.com>
2024-01-28 15:41:47 +0700

    Update fix meta viewport bug
    Update fix meta viewport bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,11 +18,11 @@ const App = ({ Component, pageProps }) => {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
-        <meta
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
+        /> */}
         <link
           rel="shortcut icon"
           type="image/x-icon"
